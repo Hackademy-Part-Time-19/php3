9484a00477a7da671fed76b5e4ad7a603ead87f4
Naizky <samulomba67@gmail.com>
2024-01-16 21:55:15 +0100

    Esercizi finiti php3

--- PrimoEsercizio.php
+++ PrimoEsercizio.php
@@ -6,10 +6,13 @@ class Company {
     public $tot_employees = 0;
     public static $avg_wag = 1500;
     public static $tot_wag = 0;
-    public contatore = 0 ;
+    public static $counter_employess = 0;
+    public static $TotalCompany = 0;
     
-    public function __construct($name, $location, $tot_employees=0){
-        
+    
+    public function __construct($name, $location, $tot_employees){
+        self::$counter_employess += $tot_employees;
+        self::$TotalCompany++;
         $this->name = $name;
         $this->location = $location;
         $this->tot_employees = $tot_employees;
@@ -30,7 +33,7 @@ class Company {
     }
 
     public function AvgWag(){
-
+        
         
         $annual_expense = $this->tot_employees * self::$avg_wag;
         self::$tot_wag += $annual_expense;
@@ -38,20 +41,20 @@ class Company {
     
     }
     public static function calculateTotals(){
-        
+        self::$tot_wag= self::$counter_employess * self::$avg_wag*12;
+        echo 'il totale dei dipendenti è ' . self::$counter_employess . ' il totali delle aziende è ' . self::$TotalCompany . ' la spesa annuale è ' . self::$tot_wag . ' euro';
 
     }
-    echo "Totale dipendenti di tutte le aziende: {$tot_employees}\n";
-    echo "Totale spese annuali di tutte le aziende: {$tot_wag} euro\n";
+    
    
-}}
+}
 $Azienda1 = new Company('Google', 'Dublino', 1000 );
 $Azienda2 = new Company('Facebook','Francoforte',2000 );
 $Azienda3 = new Company('Netflix', 'Milano', 3000 );
 $Azienda4= new Company('Apple', 'Roma', 4000 );
 $Azienda5 = new Company('Amazon', 'Torino', 5000 );
 
-
+$Azienda1->AvgWag();
 Company::calculateTotals();
 
 //$Azienda1->Hello();
